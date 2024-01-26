7db645c61b72a5bdaf6484857aab73e90681b178
nhan.nguyen <truongnhan0311@gmail.com>
2024-01-26 11:12:15 +0700

    test local

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build -f Dockerfile -t="local-python-lambda:3.8" .
+docker build -t="truongnhan0311/ilogis:local-python-lambda-3.8" .
+docker push truongnhan0311/ilogis:local-python-lambda-3.8
